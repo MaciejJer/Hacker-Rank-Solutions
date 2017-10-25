abf4b4c684c8eaea2c4d1f0fc537be2a2655147d
Maciej <motey@tut.by>
2017-10-25 11:59:51 +0200

    project rebuild with Google Checkstyle

--- src/main/java/com/hackerrank/java8/StdIN_StdOUT.java
+++ src/main/java/com/hackerrank/java8/StdIN_StdOUT.java
@@ -3,13 +3,14 @@ package com.hackerrank.java8;
 import java.util.*;
 
 public class StdIN_StdOUT {
-    public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        int n = 1;
-        while (stdIn.hasNextLine()) {
-            String line = stdIn.nextLine();
-            System.out.println(n + " " + line);
-            n++;
-        }
+
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    int n = 1;
+    while (stdIn.hasNextLine()) {
+      String line = stdIn.nextLine();
+      System.out.println(n + " " + line);
+      n++;
     }
+  }
 }

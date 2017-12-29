21472d8b15b2cd68d20679da040895d470f41afc
Maciej <motey@tut.by>
2017-12-29 23:45:57 +0100

    MIGRATORY BIRDS

--- src/main/java/com/hackerrank/algorithms/implementation/MigratoryBirds.java
+++ src/main/java/com/hackerrank/algorithms/implementation/MigratoryBirds.java
@@ -1,25 +1,36 @@
 package com.hackerrank.algorithms.implementation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class MigratoryBirds {
 
-    static int migratoryBirds(int n, int[] ar) {
-        int result =0;
-        for (int i =1 ;: ar){
+  private static int migratoryBirds(int[] ar) {
+    List<Integer> typesQuantity = new ArrayList<>();
+    IntStream.range(1, 6).forEach(i -> {
+      typesQuantity.add(((int) Arrays
+          .stream(ar)
+          .filter(num -> num == i)
+          .count()));
+    });
+    return typesQuantity.indexOf(typesQuantity
+        .stream()
+        .max(Comparator.naturalOrder())
+        .orElse(null)) + 1;
+  }
 
-        }
-        return 0;
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int[] ar = new int[n];
+    for (int ar_i = 0; ar_i < n; ar_i++) {
+      ar[ar_i] = in.nextInt();
     }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for (int ar_i = 0; ar_i < n; ar_i++) {
-            ar[ar_i] = in.nextInt();
-        }
-        int result = migratoryBirds(n, ar);
-        System.out.println(result);
-    }
+    int result = migratoryBirds(ar);
+    System.out.println(result);
+  }
 }

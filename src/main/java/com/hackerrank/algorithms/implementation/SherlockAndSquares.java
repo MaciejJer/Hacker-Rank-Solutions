460ad67438347e81722087c84d9c2c0f40b997ca
Motiejus Jermolawiczius <mjermolawiczius@bluesoft.net.pl>
2018-01-19 17:21:18 +0100

    scherlock start

--- src/main/java/com/hackerrank/algorithms/implementation/SherlockAndSquares.java
+++ src/main/java/com/hackerrank/algorithms/implementation/SherlockAndSquares.java
@@ -1,15 +1,34 @@
 package com.hackerrank.algorithms.implementation;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class SherlockAndSquares {
+
+    static Set<Integer> precalculated = new HashSet<>();
     static int squares(int a, int b) {
-        return 0;
+        int counter = 0;
+        int newA = (int)Math.sqrt((double) a);
+        int newB = (int)Math.sqrt((double) b);
+        for (int i=newA; i<=newB; i++){
+            if (precalculated.contains(i*i)){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int q = in.nextInt();
+        int squareValue = 0;
+        int value = 0;
+        while (squareValue < 1000000001){
+            squareValue = value*value;
+            precalculated.add(squareValue);
+            value++;
+        }
         for (int a0 = 0; a0 < q; a0++) {
             int a = in.nextInt();
             int b = in.nextInt();
